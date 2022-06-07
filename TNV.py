c67d6c1c8edf954d88c2f8268657a33a3e4a7da5
josmramirezmar <josmramirezmar@unal.edu.co>
2022-06-07 17:26:58 -0500

    implementacion previa

--- TNV.py
+++ TNV.py
@@ -13,14 +13,14 @@ root = logging.getLogger()
 root.setLevel(logging.INFO)
 handler = logging.StreamHandler(sys.stdout)
 handler.setLevel(logging.DEBUG)
-fh = logging.FileHandler(dirPath + "\\TVNfdsn.log")
+fh = logging.FileHandler(dirPath + "\\TVN.log")
 fh.setLevel(logging.DEBUG)
 formatter = logging.Formatter("%(asctime)s.%(msecs)03d %(levelname)s %(message)s","%Y/%m/%d %H:%M:%S")
 handler.setFormatter(formatter)
 fh.setFormatter(formatter)
 root.addHandler(handler)
 root.addHandler(fh)
-with open(dirPath + "\\TVNfdsn.log", "a") as file_object:
+with open(dirPath + "\\TVN.log", "a") as file_object:
     file_object.write("\n")
 logging.info('***MONITOREO, IDENTIFICACION y LOCALIZACION de Tremors No Volcanicos (TNV)***')
 ###############################################################################
@@ -51,7 +51,55 @@ except Exception as e:
 
 ######################### Funcion principal ###################################
 def main():
+    # Declaracion de Variables
+    #logging.info("Cargando inventario...")
+    inv = read_inventory(".\\archivo\\CM.CDT.01.dataless")
+    #logging.info("Inventario cargado.")
+    #queueR = queue()
     
+    # Estaciones del Servicio Geologico Colombiano (SGC)
+    #clientCM = Client('http://sismo.sgc.gov.co:8080/')
+    #network = "CM"
+    #stations = ["CHI","NOR","ROSC","RUS","CVER","VIL","SJC","ZAR","PTB","GUY2C","SPBC","AGCC","VMM09","VMM05","VMM07","VMM10","LL8C","VMM11","VMM12","OCA","BRJC"]
+    #channels = "*"
+    #locIds = "*"
+    #dateStart = UTCDateTime("2022-06-01T00:00:00")
+    
+    # Carpeta de Almacenamiento
+   # arcFolder = "G:\\Mi unidad\\RSUNAL\\TNV\\Python\\TNV\\archive\\"
+
+    # Inicio de hilos
+    #downloadDataThread = Thread(target=downloadData, args=(clientCM,network,stations,channels,locIds,dateStart,arcFolder,))
+    #downloadDataThread.daemon = True
+    #downloadDataThread.start()
+    #ReadDataThread = Thread(target=readData, args=(clientCM,network,stations,channelsR,locIds,timeStart,))
+    #ReadDataThread.daemon = True
+    #ReadDataThread.start()
+
+    #st = read("G:\\Mi unidad\\RSUNAL\\CDT\\Python\\TNV\\CM\\2022\\04\\01\\CM_CHI_2022-04-01.mseed")
+    #st.plot(method='full', equal_scale=False)
+    #logging.info("Impreso RAW...")
+    
+    #st.detrend()
+    #st.plot(method='full', equal_scale=False)
+    #logging.info("Impreso DETREND...")
+    
+    #st.filter("bandpass", freqmin=1, freqmax=10, corners=2, zerophase=True)
+    #st.plot(method='full', equal_scale=False)
+    #logging.info("Impreso FILTER...")
+    
+    #st.normalize(global_max=False) 
+    #st.plot(method='full', equal_scale=False)
+    #logging.info("Impreso NORMALIZE...")
+    
+    #st.detrend("linear")
+    #logging.info("Impreso DETREND...") 
+    
+    # Figura mapa de las estaciones
+    fig1 = plt.figure("Ortho")
+    inv.plot(projection='ortho', method='cartopy', show=False, fig="fig1") # global # local
+    fig1.show()
+    inv.plot(projection='global', method='cartopy', show=False, fig="fig2") # global # local
     
     while True:
         time.sleep(5)
